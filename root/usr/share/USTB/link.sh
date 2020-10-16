6f28d3ac6acbabf3a7908fc1125d17a52cd6542c
WROIATE <j.wroiate@gmail.com>
2020-10-16 22:07:01 +0800

    更新重连模式

--- root/usr/share/USTB/link.sh
+++ root/usr/share/USTB/link.sh
@@ -9,6 +9,22 @@ TEST_V4URL=$(uci get ustb.@advance[0].test_v4url)
 TEST_V6URL=$(uci get ustb.@advance[0].test_v6url)
 Login_IP=$(uci get ustb.@advance[0].login_ip)
 Error=0
+Enable=1
+
+connection() {
+    if [ "$V6" == "1" ]; then
+        ipv6=$(ip -6 address show | grep '2001:da8:208' | awk '{print $2}' | cut -d '/' -f1)
+        [ "$ipv6" != "" ] && echo "[$(date +'%Y-%m-%d %H:%M:%S')] IPV6 Address:[${ipv6}]" >>$log_file || echo "[$(date +'%Y-%m-%d %H:%M:%S')] IPV6 Address Can't be found" >>$log_file
+    else
+        ipv6=""
+    fi
+    $(curl -sd "DDDDD=${USER}&upass=${PASSWORD}&v6ip=${ipv6}&0MKKey=123456789" ${Login_IP}) >/dev/null
+}
+
+check() {
+    [ $(curl -I -m 4 -o /dev/null -s -w %{http_code}"\n" "${URL}") == 200 ] && Error=0 || Error=1
+}
+
 if [ "$V6" == "1" ]; then
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] Check Connections with V6" >>$log_file
     [ "$TEST_V6URL" != "" ] && URL=$TEST_V6URL || URL='http://cippv6.ustb.edu.cn/get_ip.php'
@@ -16,20 +32,18 @@ else
     echo "[$(date +'%Y-%m-%d %H:%M:%S')] Check Connections with V4" >>$log_file
     [ "$TEST_V4URL" != "" ] && URL=$TEST_V4URL || URL='https://www.baidu.com/'
 fi
+
 while true; do
     file_size=$(du -a /root/link.log | awk '{print $1}')
-    [ $(curl -I -m 4 -o /dev/null -s -w %{http_code}"\n" "${URL}") == 200 ] && Error=0 || Error=1
+    check
     if [ $Error != 0 ]; then
         echo "[$(date +'%Y-%m-%d %H:%M:%S')] Curl failed, now will try to connect USTB network" >>$log_file
-        if [ "$V6" == "1" ]; then
-            ipv6=$(ip -6 address show | grep '2001:da8:208' | awk '{print $2}' | cut -d '/' -f1)
-            [ "$ipv6" != "" ] && echo "[$(date +'%Y-%m-%d %H:%M:%S')] IPV6 Address:[${ipv6}]" >>$log_file || echo "[$(date +'%Y-%m-%d %H:%M:%S')] IPV6 Address Can't be found" >>$log_file
-        else
-            ipv6=""
-        fi
-        $(curl -sd "DDDDD=${USER}&upass=${PASSWORD}&v6ip=${ipv6}&0MKKey=123456789" ${Login_IP}) >/dev/null
+        while [ $Error != 0 ]; do
+            connection
+            sleep 3
+            check
+        done
         echo "[$(date +'%Y-%m-%d %H:%M:%S')] Connecting to ${Login_IP}" >>$log_file
-        sleep 3
     else
         if [ $(expr $num % 100) == 0 ]; then
             echo "[$(date +'%Y-%m-%d %H:%M:%S')] The Connection is normal:${num}" >>$log_file
