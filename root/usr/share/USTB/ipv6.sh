09145dd999f2fb89fc2423cd9fe7350d64c95443
Jarao <44677306+WROIATE@users.noreply.github.com>
2020-11-20 10:34:21 +0800

    修改换行错误

--- root/usr/share/USTB/ipv6.sh
+++ root/usr/share/USTB/ipv6.sh
@@ -8,7 +8,7 @@ sed -i "/# The following two rules is Nat6 for USTB/d" /etc/firewall.user
 
 if [ "$Gateway" != "" ] && [ "$Eth" != "" ]; then
 	echo "[$(date +'%Y-%m-%d %H:%M:%S')] Got IPV6 Net Interface ${Eth} and Gateway ${Gateway}" >>/usr/share/USTB/link.log
-	echo -e "/n# The following two rules added by Nat6 setting of USTB, you can remove them by yourself" >>/etc/firewall.user
+	echo -e "\n# The following two rules added by Nat6 setting of USTB, you can remove them by yourself" >>/etc/firewall.user
 	echo "ip -6 r add default via ${Gateway} dev ${Eth}" >>/etc/firewall.user
 	echo "ip6tables -t nat -A POSTROUTING -o ${Eth} -j MASQUERADE" >>/etc/firewall.user
 	/etc/init.d/firewall restart >/dev/null
